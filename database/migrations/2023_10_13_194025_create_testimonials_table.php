0c4a3c5c002bcc1e9356d85d9533b90117e478cf
JonathanItz <jonathanitzen95@gmail.com>
2023-10-13 14:44:45 -0500

    save

---
+++ database/migrations/2023_10_13_194025_create_testimonials_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 1024);
+            $table->string('email', 1024)->nullable();
+            $table->text('testimonial');
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testimonials');
+    }
+};
