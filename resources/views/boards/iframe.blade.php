0930d8c96f36de1167d70865593ff24f07d9cb0c
JonathanItz <jonathanitzen95@gmail.com>
2023-10-23 11:39:43 -0500

    save

--- resources/views/boards/iframe.blade.php
+++ resources/views/boards/iframe.blade.php
@@ -1,24 +1,6 @@
-<x-board-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot> --}}
-
-    @if ($logoUrl)
-        <div class="flex justify-center pt-8">
-            @if ($websiteUrl)
-                <a href="{{$websiteUrl}}">
-                    <x-company-logo logoUrl="{{$logoUrl}}" />
-                </a>
-            @else
-                <x-company-logo logoUrl="{{$logoUrl}}" />
-            @endif
-        </div>
-    @endif
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto md:px-6 lg:px-8">
+<x-iframe-layout>
+    <div>
+        <div>
             @if (! $testimonails->isEmpty())
                 <div
                     id="grid-container"
