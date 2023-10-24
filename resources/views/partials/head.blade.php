940eb061802446911a49288f944d265450ec28eb
JonathanItz <jonathanitzen95@gmail.com>
2023-10-24 13:36:33 -0500

    save

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Lemon Bestie') }}</title>
+
+    <link rel="icon" type="image/x-icon" href="{{asset('/assets/images/logo.png')}}">
+    
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @stack('header-scripts')
+</head>
