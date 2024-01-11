2325c97a9fa1833c95850981dbcba8c0abc3e8de
JonathanItz <jonathanitzen95@gmail.com>
2024-01-11 16:46:52 -0600

    save

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,15 +10,18 @@
             <div class="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-primary/10 ring-1 ring-primary sm:-mr-80 lg:-mr-96" aria-hidden="true"></div>
             <div class="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
                 <div class="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-                <h1 class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">Lemon Bestie is changing the way people connect.</h1>
-                <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-                    <p class="text-lg leading-8 text-gray-600">Unlock the power of authentic testimonials with Lemon Bestie! We understand the impact that genuine feedback can have on your business, and that's why we've created a platform to make collecting and showcasing testimonials a breeze.</p>
-                    <div class="mt-10 flex items-center gap-x-6">
-                    <a href="{{route('register')}}" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">Get started</a>
-                    <a href="{{route('login')}}" class="text-sm font-semibold leading-6 text-gray-900 focus-visible:outline-secondary">Login <span aria-hidden="true">→</span></a>
+                    <div>
+                        <h1 class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">Lemon Bestie</h1>
+                        <h2 class="max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:col-span-2 xl:col-auto">Elevate Connections with Authentic Testimonials</h2>
                     </div>
-                </div>
-                <img src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80" alt="" class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36">
+                    <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+                        <p class="text-lg leading-8 text-gray-600">Unlock the power of authentic testimonials with Lemon Bestie! We understand the impact that genuine feedback can have on your business, and that's why we've created a platform to make collecting and showcasing testimonials a breeze.</p>
+                        <div class="mt-10 flex items-center gap-x-6">
+                        <a href="{{route('register')}}" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary">Get started</a>
+                        <a href="{{route('login')}}" class="text-sm font-semibold leading-6 text-gray-900 focus-visible:outline-secondary">Login <span aria-hidden="true">→</span></a>
+                        </div>
+                    </div>
+                    <img src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80" alt="" class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36">
                 </div>
             </div>
         <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"></div>
