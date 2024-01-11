d0546fbe2c27a26ba27ca879356584cf1b718eb6
JonathanItz <jonathanitzen95@gmail.com>
2024-01-11 11:19:43 -0600

    save

--- app/View/Components/IFrameLayout.php
+++ app/View/Components/IFrameLayout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class IFrameLayout extends Component
+class IframeLayout extends Component
 {
     /**
      * Get the view / contents that represent the component.
--- resources/views/layouts/iframe.blade.php
+++ resources/views/layouts/iframe.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+
     @include('partials.head')
 
     <body class="font-sans antialiased">
+        <!-- Page Content -->
         <main>
             {{ $slot }}
         </main>
 
-        @livewireScripts
+    @livewireScripts
     </body>
 </html>
 
