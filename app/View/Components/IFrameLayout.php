0e1e644b02a6420c9f5cc6dfb5042cf374a52879
JonathanItz <jonathanitzen95@gmail.com>
2024-01-11 11:30:33 -0600

    save

--- app/View/Components/IFrameLayout.php
+++ app/View/Components/IFrameLayout.php
@@ -2,18 +2,16 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\View\View;
 
 class IframeLayout extends Component
 {
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View|string
     {
-        dd('yaya');
         return view('layouts.iframe');
     }
 }
