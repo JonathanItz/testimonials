8c82adcc9119071c3ce0e56f0a3ac38c378b4719
JonathanItz <jonathanitzen95@gmail.com>
2024-01-10 21:13:29 -0600

    save

--- app/View/Components/IFrameLayout.php
+++ app/View/Components/IFrameLayout.php
@@ -5,7 +5,7 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
-class IFrameLayout extends Component
+class IframeLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
