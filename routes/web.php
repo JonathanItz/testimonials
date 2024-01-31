5015e11e29cb20784638f25d977d8e3f0bdef15a
JonathanItz <jonathanitzen95@gmail.com>
2024-01-31 14:20:52 -0600

    save

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Models\Testimonial;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('{any}', function () {
+    return view('comingsoon');
+})->where('any', '.*');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,197 +20,197 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')
-    ->name('home');
+// Route::view('/', 'welcome')
+//     ->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified', 'hasboard'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified', 'hasboard'])
+//     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth', 'hasboard'])
-    ->name('profile');
+// Route::view('profile', 'profile')
+//     ->middleware(['auth', 'hasboard'])
+//     ->name('profile');
 
-Route::view('/terms', 'terms')
-    ->name('terms');
+// Route::view('/terms', 'terms')
+//     ->name('terms');
 
-// Route::get('create-board', function() {
-//     return view('boards/create');
+// // Route::get('create-board', function() {
+// //     return view('boards/create');
+// // })
+// // ->middleware(['auth'])
+// // ->name('board.create');
+
+// Route::middleware(['notSubscribed'])->group(function () {
+//     Route::get('/board/{slug}', function($slug, Request $request) {
+//         $board = Board::where('slug', $slug)
+//             ->firstOrFail();
+    
+//         $testimonails = $board->testimonials()
+//             ->where('status', 'accepted')
+//             ->orderBy('created_at', 'desc')
+//             ->limit($request->get('maxTestimonials'))
+//             ->get();
+    
+//         $logoUrl = '';
+//         $logo = $board->getFirstMedia('companylogo');
+//         if($logo) {
+//             $logoUrl = $board->getFirstMedia('companylogo')->getFullUrl();
+//         }
+    
+//         $websiteUrl = '';
+//         if(isset($board?->settings['website']) && $board?->settings['website']) {
+//             $websiteUrl = 'https://'.$board?->settings['website'];
+//         }
+    
+//         $testimonialSettings = $board?->settings['testimonials'];
+
+//         $border = 'border';
+//         if(isset($testimonialSettings['border'])) {
+//             $border = $testimonialSettings['border'];
+//         }
+
+//         $borderColor = '#e5e7eb';
+//         if(isset($testimonialSettings['borderColor'])) {
+//             $borderColor = $testimonialSettings['borderColor'];
+//         }
+
+//         $radius = 'rounded-xl';
+//         if(isset($testimonialSettings['radius'])) {
+//             $radius = $testimonialSettings['radius'];
+//         }
+
+//         $shadow = 'shadow-md';
+//         if(isset($testimonialSettings['shadow'])) {
+//             $shadow = $testimonialSettings['shadow'];
+//         }
+    
+//         return view('boards.board', [
+//             'name' => $board->name,
+//             'slug' => $board->slug,
+//             'testimonails' => $testimonails,
+//             'logoUrl' => $logoUrl,
+//             'websiteUrl' => $websiteUrl,
+//             'radius' => $radius,
+//             'shadow' => $shadow,
+//             'border' => $border,
+//             'borderColor' => $borderColor,
+//         ]);
+//     })
+//     ->name('board');
+    
+//     Route::get('/iframe/{slug}', function($slug, Request $request) {
+//         $board = Board::where('slug', $slug)
+//             ->firstOrFail();
+    
+//         $testimonails = $board->testimonials()
+//             ->where('status', 'accepted')
+//             ->orderBy('created_at', 'desc')
+//             ->limit($request->get('maxTestimonials'))
+//             ->get();
+//         $isIframe = true;
+
+//         $testimonialSettings = $board?->settings['testimonials'];
+
+//         $border = 'border';
+//         if(isset($testimonialSettings['border'])) {
+//             $border = $testimonialSettings['border'];
+//         }
+
+//         $borderColor = '#e5e7eb';
+//         if(isset($testimonialSettings['borderColor'])) {
+//             $borderColor = $testimonialSettings['borderColor'];
+//         }
+
+//         $radius = 'rounded-xl';
+//         if(isset($testimonialSettings['radius'])) {
+//             $radius = $testimonialSettings['radius'];
+//         }
+
+//         $shadow = 'shadow-md';
+//         if(isset($testimonialSettings['shadow'])) {
+//             $shadow = $testimonialSettings['shadow'];
+//         }
+    
+//         return view('boards.iframe', [
+//             'slug' => $board->slug,
+//             'testimonails' => $testimonails,
+//             'isIframe' => $isIframe,
+//             'radius' => $radius,
+//             'shadow' => $shadow,
+//             'border' => $border,
+//             'borderColor' => $borderColor,
+//         ]);
+//     })
+//     ->name('board.iframe');
+// });
+
+// Route::get('/form/{slug}', function($slug) {
+//     $board = Board::where('slug', $slug)
+//         ->firstOrFail();
+
+//     $logoUrl = '';
+//     $logo = $board->getFirstMedia('companylogo');
+//     if($logo) {
+//         $logoUrl = $board->getFirstMedia('companylogo')->getFullUrl();
+//     }
+
+//     $websiteUrl = '';
+//     if(isset($board?->settings['website']) && $board?->settings['website']) {
+//         $websiteUrl = 'https://'.$board?->settings['website'];
+//     }
+
+//     $limit = 1000;
+//     if(isset($board?->settings['testimonials']['limit']) && $board?->settings['testimonials']['limit']) {
+//         $limit = +$board?->settings['testimonials']['limit'];
+//     }
+
+//     return view('boards.form', [
+//         'boardId' => $board->id,
+//         'logoUrl' => $logoUrl,
+//         'websiteUrl' => $websiteUrl,
+//         'limit' => $limit
+//     ]);
 // })
-// ->middleware(['auth'])
-// ->name('board.create');
+// ->name('board.form');
 
-Route::middleware(['notSubscribed'])->group(function () {
-    Route::get('/board/{slug}', function($slug, Request $request) {
-        $board = Board::where('slug', $slug)
-            ->firstOrFail();
-    
-        $testimonails = $board->testimonials()
-            ->where('status', 'accepted')
-            ->orderBy('created_at', 'desc')
-            ->limit($request->get('maxTestimonials'))
-            ->get();
-    
-        $logoUrl = '';
-        $logo = $board->getFirstMedia('companylogo');
-        if($logo) {
-            $logoUrl = $board->getFirstMedia('companylogo')->getFullUrl();
-        }
-    
-        $websiteUrl = '';
-        if(isset($board?->settings['website']) && $board?->settings['website']) {
-            $websiteUrl = 'https://'.$board?->settings['website'];
-        }
-    
-        $testimonialSettings = $board?->settings['testimonials'];
+// Route::get('/testimonial/edit/{testimonial:id}', function(Testimonial $testimonial) {
+//     $board = $testimonial->board()->first();
+//     $user = auth()->user();
+//     $userId = $user->id;
 
-        $border = 'border';
-        if(isset($testimonialSettings['border'])) {
-            $border = $testimonialSettings['border'];
-        }
+//     if($userId !== $board->id) {
+//         return abort(404);
+//     }
 
-        $borderColor = '#e5e7eb';
-        if(isset($testimonialSettings['borderColor'])) {
-            $borderColor = $testimonialSettings['borderColor'];
-        }
+//     $limit = 1000;
+//     if(isset($board?->settings['testimonials']['limit']) && $board?->settings['testimonials']['limit']) {
+//         $limit = +$board?->settings['testimonials']['limit'];
+//     }
 
-        $radius = 'rounded-xl';
-        if(isset($testimonialSettings['radius'])) {
-            $radius = $testimonialSettings['radius'];
-        }
+//     return view('testimonials.edit', [
+//         'testimonial' => $testimonial,
+//         'limit' => $limit,
+//     ]);
+// })
+// ->middleware(['auth', 'verified'])
+// ->name('testimonial.edit');
 
-        $shadow = 'shadow-md';
-        if(isset($testimonialSettings['shadow'])) {
-            $shadow = $testimonialSettings['shadow'];
-        }
-    
-        return view('boards.board', [
-            'name' => $board->name,
-            'slug' => $board->slug,
-            'testimonails' => $testimonails,
-            'logoUrl' => $logoUrl,
-            'websiteUrl' => $websiteUrl,
-            'radius' => $radius,
-            'shadow' => $shadow,
-            'border' => $border,
-            'borderColor' => $borderColor,
-        ]);
-    })
-    ->name('board');
-    
-    Route::get('/iframe/{slug}', function($slug, Request $request) {
-        $board = Board::where('slug', $slug)
-            ->firstOrFail();
-    
-        $testimonails = $board->testimonials()
-            ->where('status', 'accepted')
-            ->orderBy('created_at', 'desc')
-            ->limit($request->get('maxTestimonials'))
-            ->get();
-        $isIframe = true;
+// Route::get('/settings/{slug}', function($slug) {
+//     $user = auth()->user();
+//     $userId = $user->id;
 
-        $testimonialSettings = $board?->settings['testimonials'];
+//     $board = Board::where('slug', $slug)
+//         ->firstOrFail();
 
-        $border = 'border';
-        if(isset($testimonialSettings['border'])) {
-            $border = $testimonialSettings['border'];
-        }
+//     if($userId !== $board->user_id) {
+//         return abort(404);
+//     }
 
-        $borderColor = '#e5e7eb';
-        if(isset($testimonialSettings['borderColor'])) {
-            $borderColor = $testimonialSettings['borderColor'];
-        }
+//     return view('boards.settings', [
+//         'board' => $board
+//     ]);
+// })
+// ->middleware(['auth', 'verified'])
+// ->name('boards.settings');
 
-        $radius = 'rounded-xl';
-        if(isset($testimonialSettings['radius'])) {
-            $radius = $testimonialSettings['radius'];
-        }
-
-        $shadow = 'shadow-md';
-        if(isset($testimonialSettings['shadow'])) {
-            $shadow = $testimonialSettings['shadow'];
-        }
-    
-        return view('boards.iframe', [
-            'slug' => $board->slug,
-            'testimonails' => $testimonails,
-            'isIframe' => $isIframe,
-            'radius' => $radius,
-            'shadow' => $shadow,
-            'border' => $border,
-            'borderColor' => $borderColor,
-        ]);
-    })
-    ->name('board.iframe');
-});
-
-Route::get('/form/{slug}', function($slug) {
-    $board = Board::where('slug', $slug)
-        ->firstOrFail();
-
-    $logoUrl = '';
-    $logo = $board->getFirstMedia('companylogo');
-    if($logo) {
-        $logoUrl = $board->getFirstMedia('companylogo')->getFullUrl();
-    }
-
-    $websiteUrl = '';
-    if(isset($board?->settings['website']) && $board?->settings['website']) {
-        $websiteUrl = 'https://'.$board?->settings['website'];
-    }
-
-    $limit = 1000;
-    if(isset($board?->settings['testimonials']['limit']) && $board?->settings['testimonials']['limit']) {
-        $limit = +$board?->settings['testimonials']['limit'];
-    }
-
-    return view('boards.form', [
-        'boardId' => $board->id,
-        'logoUrl' => $logoUrl,
-        'websiteUrl' => $websiteUrl,
-        'limit' => $limit
-    ]);
-})
-->name('board.form');
-
-Route::get('/testimonial/edit/{testimonial:id}', function(Testimonial $testimonial) {
-    $board = $testimonial->board()->first();
-    $user = auth()->user();
-    $userId = $user->id;
-
-    if($userId !== $board->id) {
-        return abort(404);
-    }
-
-    $limit = 1000;
-    if(isset($board?->settings['testimonials']['limit']) && $board?->settings['testimonials']['limit']) {
-        $limit = +$board?->settings['testimonials']['limit'];
-    }
-
-    return view('testimonials.edit', [
-        'testimonial' => $testimonial,
-        'limit' => $limit,
-    ]);
-})
-->middleware(['auth', 'verified'])
-->name('testimonial.edit');
-
-Route::get('/settings/{slug}', function($slug) {
-    $user = auth()->user();
-    $userId = $user->id;
-
-    $board = Board::where('slug', $slug)
-        ->firstOrFail();
-
-    if($userId !== $board->user_id) {
-        return abort(404);
-    }
-
-    return view('boards.settings', [
-        'board' => $board
-    ]);
-})
-->middleware(['auth', 'verified'])
-->name('boards.settings');
-
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
