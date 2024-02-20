c433dca46ddd983b33f51af4d54cabb9e55103a2
JonathanItz <jonathanitzen95@gmail.com>
2024-02-20 09:53:08 -0600

    save

--- resources/views/boards/iframe.blade.php
+++ resources/views/boards/iframe.blade.php
@@ -2,5 +2,5 @@
     <div class="px-4 min-h-[4rem] h-full">
         @include('partials.testimonials.container')
     </div>
-    <div class="h-9"></div>
+    <div class="h-[.7rem] md:h-9"></div>
 </x-frame-layout>
